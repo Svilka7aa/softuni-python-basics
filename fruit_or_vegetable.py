914865cc8d8721dd9d124810ee21f3fc572ed1b6
Svilen Petrov <102098862+Svilka7aa@users.noreply.github.com>
2023-01-17 21:04:27 +0100

    Add files via upload

---
+++ fruit_or_vegetable.py
@@ -0,0 +1,13 @@
+objekt = input()
+
+if objekt == "banana" \
+        or objekt == "apple" \
+        or objekt == "kiwi" \
+        or objekt == "lemon" \
+        or objekt == "grapes" \
+        or objekt == "cherry":
+    print("fruit")
+elif objekt == "tomato" or objekt == "cucumber" or objekt == "pepper" or objekt == "carrot":
+    print("vegetable")
+else:
+    print("unknown")
